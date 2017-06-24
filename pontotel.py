f7284c0da1c93ae43997dd77c589e0eb4b433c01
Mariana Bisca <mariana.bisca@gmail.com>
2017-06-24 13:03:09 -0300

    first commit

---
+++ pontotel.py
@@ -0,0 +1,21 @@
+import hug
+import requests
+
+
+@hug.local()
+def get_occurrences(url: hug.types.text, key_word: hug.types.text):
+    """
+    Returns JSON containing number of occurrences of "key_word" in website
+    given by "url"
+    """
+    content = requests.get(url).read()
+    html_text = content.decode('utf-8')
+    from bs4 import BeautifulSoup
+    print (BeautifulSoup(html_text, "lxml").get_text())
+    text = BeautifulSoup(html_text, "lxml").get_text().split()
+    counter = 0
+    for word in text:
+        if word == key_word:
+            counter += 1
+    string = '{{"{0}": "{1}"}}'.format("Occurrences", counter)
+    return (string)
