e3f54fb437271dc780ac803c12d229e40cc91e12
Mariana Bisca <mariana.bisca@gmail.com>
2017-06-24 23:38:43 -0300

    works if keyword isn't used next to punctuation

--- pontotel.py
+++ pontotel.py
@@ -1,21 +1,31 @@
 import hug
 import requests
 
-
 @hug.local()
 def get_occurrences(url: hug.types.text, key_word: hug.types.text):
     """
     Returns JSON containing number of occurrences of "key_word" in website
     given by "url"
     """
+    # get and decode web content
     content = requests.get(url).content
     html_text = content.decode('utf-8')
+
+    # remove Javascript, CSS and HTML
     from bs4 import BeautifulSoup
-    #print (BeautifulSoup(html_text, "lxml").get_text())
-    text = BeautifulSoup(html_text, "lxml").get_text().split()
+    soup = BeautifulSoup(html_text, "lxml")
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+    text = soup.get_text()
+
+    # print(text) #for testing
+    # split text into iterable list of words
+    text = text.split()
+
+    # count keyword occurrences
     counter = 0
     for word in text:
         if word == key_word:
             counter += 1
-    string = '{{"{0}": "{1}"}}'.format("Occurrences", counter)
-    return (string)
+
+    return ('{{"{0}": "{1}"}}'.format("Occurrences", counter))
